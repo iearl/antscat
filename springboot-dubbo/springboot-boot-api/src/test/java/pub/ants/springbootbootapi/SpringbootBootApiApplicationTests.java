588fc787782e1b8b21397408d2ee16c7e3cbab7f
iearl <dc_magaowei@163.com>
2019-08-25 16:36:03 +0800

    修改文件

---
+++ springboot-dubbo/springboot-boot-api/src/test/java/pub/ants/springbootbootapi/SpringbootBootApiApplicationTests.java
@@ -0,0 +1,16 @@
+package pub.ants.springbootbootapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootBootApiApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
