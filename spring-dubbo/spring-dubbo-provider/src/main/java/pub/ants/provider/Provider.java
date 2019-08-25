705899d51b3f9cc39659251fd5dc4ba9c423c6f0
iearl <dc_magaowei@163.com>
2019-08-25 20:39:33 +0800

    采用zk注册中心

--- spring-dubbo/spring-dubbo-provider/src/main/java/pub/ants/provider/Provider.java
+++ spring-dubbo/spring-dubbo-provider/src/main/java/pub/ants/provider/Provider.java
@@ -3,6 +3,7 @@ package pub.ants.provider;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Provider {
+
     public static void main(String[] args) throws Exception {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("classpath:provider.xml");
         context.start();
